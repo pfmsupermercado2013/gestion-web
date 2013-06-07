d43ce87a1ef93937ed2a7d3c3bc254e0a13c5583
ÓscarJavier <scarJavier@JavierFquene>
2013-06-07 04:24:33 +0200

    Gestión empleados

--- Backend-Supermercado/Backend-Supermercado-war/src/java/org/back/servlets/LoginServlet.java
+++ Backend-Supermercado/Backend-Supermercado-war/src/java/org/back/servlets/LoginServlet.java
@@ -5,11 +5,21 @@
 package org.back.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.back.constants.BackConstantes;
+import org.back.ejb.GestionEmpleadosEjbLocal;
+import org.back.ejb.GestionSupermercadoEjbLocal;
+import org.back.hibernate.model.Empleado;
+import org.back.hibernate.model.Supermercado;
+import org.back.utils.PasswordEncoder;
 
 /**
  *
@@ -18,6 +28,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class LoginServlet extends HttpServlet {
+    
+    @EJB
+    private GestionEmpleadosEjbLocal gestionEmpleadosEjbLocal;
+    
+    @EJB
+    private GestionSupermercadoEjbLocal gestionSupermercadoEjbLocal;
 
     /**
      * Processes requests for both HTTP
@@ -31,24 +47,51 @@ public class LoginServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-            
-            response.sendRedirect("principal.jsp");
-        } finally {            
-            out.close();
-        }
+        response.setContentType("text/html;charset=iso-8859-15");
+        HttpSession session = null;
+        Empleado empleado = null;
+        String operacion  = "";
+        String rol        = "";
+        String idUsuario  = "";
+        String password   = "";
+        operacion = request.getParameter("cmd");
+        rol = "";
+        idUsuario = "";
+        password = "";
+        // Si el usuario es Administrador
+        if (operacion.equals(BackConstantes.LOGIN_ADMIN)){
+            rol = request.getParameter("rol");
+            idUsuario = request.getParameter("admin-id");
+            password = request.getParameter("admin-pass");
+            empleado = loginAdmin(rol, idUsuario, password);
+            if(empleado != null) {
+                session = request.getSession(true);  
+                response.sendRedirect("principal.jsp");
+            }
+        } else  // Si el usuario es empleado
+            if (operacion.equals(BackConstantes.LOGIN_NORMAL)){
+                idUsuario = request.getParameter("idUsuario");
+                password = request.getParameter("passUsuario");
+                 try {
+                    PasswordEncoder encoder = PasswordEncoder.getInstance();
+                    password = encoder.encode(password, BackConstantes.SALT_KEY);
+                    empleado = login(idUsuario, password);
+                 } catch (Exception ex) {
+                    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+                 if (empleado != null) {
+                    session = request.getSession(true); 
+                    session.setAttribute("empleado", empleado);  
+                    response.sendRedirect("principal.jsp");
+                 } else {
+                    request.setAttribute("error-acceso", "Acceso no permitido.");
+                    request.getRequestDispatcher("login.jsp").forward(request, response); 
+                 }
+            } else // Si se cierra sesión 
+                if (operacion.equals(BackConstantes.LOGOUT)) {
+                  logout(session);
+                  response.sendRedirect("login.jsp");
+                }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -81,14 +124,67 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    
+    private Empleado loginAdmin(String rol, String idUsuario, String password){
+        Empleado empleado = null;
+        String passwordEncode = "";
+        if(BackConstantes.ROL_SUPER.equals(rol)){
+                try {
+                    PasswordEncoder encoder = PasswordEncoder.getInstance();
+                    passwordEncode = encoder.encode(password, BackConstantes.SALT_KEY);
+                    // Si el rol es de administrador, lo asociamos a un supermercado ficticio
+                    // por temas de integridad referencial en base de datos
+                    if(BackConstantes.ROL_SUPER.equals(rol)){
+                        Supermercado supermercado = new Supermercado();
+                        supermercado.setDireccionSupermercado("99");
+                        supermercado.setNombreSupermercado("supermercado-admin");
+                        supermercado.setLocalidadSupermercado("99");
+                        supermercado.setProvinciaSupermercado("99");
+                        try {
+                            supermercado = gestionSupermercadoEjbLocal.crearSupermercado(supermercado);
+                            empleado = crearEmpleadoAdmin(idUsuario, passwordEncode, supermercado);
+                        } catch (Exception ex) {
+                            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        }   
+                    }
+                } catch (NoSuchAlgorithmException ex) {
+                    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ioEx){
+                    Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ioEx);
+                }
+            } 
+        
+        return empleado;
+    }
+    
+    private Empleado login(String idUsuario, String password) throws Exception{
+        Empleado empleado = null;
+        // Comprobamos la identidad del usuario
+        empleado = gestionEmpleadosEjbLocal.validarIdentidadEmpleado(idUsuario, password);
+        
+        return empleado;
+    }
+    
+    private Empleado crearEmpleadoAdmin(String idEmpleado, String password, Supermercado supermercado){
+        Empleado empleado = new Empleado();
+        empleado.setNif(idEmpleado);
+        empleado.setPassword(password);
+        empleado.setNombreEmpleado("Admin");
+        empleado.setApellidosEmpleado("Admin");
+        empleado.setRol(BackConstantes.ROL_SUPER);
+        empleado.setEmail("pfmsupermercado2013@gmail.com");
+        empleado.setSupermercado(supermercado);
+        try {
+            gestionEmpleadosEjbLocal.crearEmpleado(empleado);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return empleado;
+    }
+    
+    private void logout(HttpSession session){
+        if(session != null)
+           session.invalidate();
+    }
 }
