4931bd0a6f603d4122a58c275ff8071f235f9153
Planetmedia <Planetmedia@192.168.1.110>
2013-06-16 11:52:32 +0200

    Publish GetProducto

--- Backend-Supermercado/Backend-Supermercado-war/src/java/org/back/rest/ProductoRestService.java
+++ Backend-Supermercado/Backend-Supermercado-war/src/java/org/back/rest/ProductoRestService.java
@@ -1,39 +1,43 @@
 package org.back.rest;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import org.back.ejb.GestionProductosEjbLocal;
 import org.back.hibernate.model.Producto;
 
 /**
  *
- * @author Alejandro Garcia
+ * @author Fernando Pabón
  */
-@Path("/ProductoRestService")
+
+@Path("/productos")
 public class ProductoRestService {
 
-   @GET
-   @Produces(MediaType.APPLICATION_JSON)
-    public List<Producto> getProductos() {
-       //creamos el array de productos
-        ArrayList<Producto> productos = new ArrayList<Producto>();
-        //creamos los productos
-        for (int i = 0; i<5; i++){
+    GestionProductosEjbLocal gestionProductosEjb = lookupGestionProductosEjbLocal();
 
-            Producto producto = new Producto();
-
-            producto.setIdproducto(i);
-            producto.setNombreProducto("producto " + i);
-            producto.setFechaEntrada(new Date());
-
-            productos.add(producto);
-        }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Producto> getProductos() throws Exception {
+        List<Producto> productos = gestionProductosEjb.listarTodosProductos(10);
         return productos;
     }
 
+
+    private GestionProductosEjbLocal lookupGestionProductosEjbLocal() {
+        try {
+            Context c = new InitialContext();
+            return (GestionProductosEjbLocal) c.lookup("java:global/Backend-Supermercado/Backend-Supermercado-ejb/GestionProductosEjb!org.back.ejb.GestionProductosEjbLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
 }
